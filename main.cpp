3f212a76508df1b473ecbe1e1f9d6d54ee38ed1f
Fernando Cruz <fernandogomescruz.com>
2021-01-07 19:20:01 -0300

    more lines and endl

--- main.cpp
+++ main.cpp
@@ -7,10 +7,13 @@
 using namespace std;
 
 int main(){
-    //c out
+    //c out - \n
     cout << "My first program in C++\n";
+    //c out - endl
+    cout << "ronaldinho soccer" << endl;
+    cout << "In addition, I am learning more about git and github and improving my English :)" << endl;
 
-    
+
     system("pause");
     return 0;
 }
